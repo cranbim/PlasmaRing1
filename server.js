1e7ebb69d19c2392ff0f557a03d6e3fae4eebeb6
cranbim <dave.webb@cranbim.com>
2016-09-29 10:44:23 +0100

    Connect and disconnect sorted

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app=express();
 
 var server=app.listen(4000);
 
+var sessions=[];
+var nextID=10000;
+
 app.use(express.static('public'));
 
 console.log("My socket server is running");
@@ -16,11 +19,31 @@ var io=socket(server);
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
-  console.log("New connection "+socket.id);
+  var session=new Session(socket);
+  sessions.push(session);
+  console.log("New connection, session:"+session.id+" socket:"+socket.id);
+  console.log("Num sessions:"+sessions.length);
   socket.on('mouse', mouseMsg);
+  socket.on('disconnect', clientDisconnect);
 
   function mouseMsg(data){
-  	console.log(data +' from '+socket.id);
-  	socket.broadcast.emit('mouse', data);
+			console.log(data +' from '+socket.id);
+			socket.broadcast.emit('mouse', data);
   }
+
+  function clientDisconnect(){
+		var i=sessions.forEach(function(sesh,index){
+			if(sesh.id==session.id) return index;
+		});
+		sessions.splice(i,1);
+		console.log("Removed disconnected session: "+session.id+" socket:"+session.socket.id);
+		console.log("Num sessions:"+sessions.length);
+	}
+}
+
+
+
+function Session(socket){ //class to hold session info
+	this.socket=socket;
+	this.id=nextID++; //increment the ID number
 }
